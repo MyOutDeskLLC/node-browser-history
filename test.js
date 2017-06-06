edafbe622b8d4ba4399214b8c1d5d1b065119af0
Lance Thompson <starrazor31@gmail.com>
2017-06-06 10:08:00 -0700

    Updating Tests to be more verbose

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 var getHistory = require("./index");
 
-getHistory().then(function (stuff) {
-    console.log(stuff);
-}).catch(function (someError) {
-    console.error(someError);
-});
+
+function singleRun() {
+    getHistory().then(function (stuff) {
+        console.log("Pass Single Run Test");
+    }).catch(function (someError) {
+        console.log("Fail Single Run Test");
+        console.error(someError);
+    });
+}
+
+function concurrencyLockTest() {
+    var pass = true;
+    for (var x = 0; x < 100; x++) {
+        getHistory().then(function (stuff) {
+        }).catch(function (someError) {
+            pass = false;
+            console.log("Fail Concurrency Lock Test");
+            console.error(someError);
+        });
+    }
+    if (pass) {
+        console.log("Pass Concurrency Lock Test");
+    }
+}
+
+singleRun();
+concurrencyLockTest();
